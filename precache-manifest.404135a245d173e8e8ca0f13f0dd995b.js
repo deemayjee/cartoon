c7d3301f07ccf6d14c15f0d78cf175db18c7356e
System Administrator <root@Dimejis-MacBook-Pro.local>
2020-06-22 21:53:25 +0100

    Updates

---
+++ precache-manifest.404135a245d173e8e8ca0f13f0dd995b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da8aab8001d93bf00ef52dc1218b3e4c",
+    "url": "/cartoon/index.html"
+  },
+  {
+    "revision": "04e268971ee8f029fd84",
+    "url": "/cartoon/static/css/main.3619ef3e.chunk.css"
+  },
+  {
+    "revision": "f492359ee70c09888d5d",
+    "url": "/cartoon/static/js/2.a1e2656b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cartoon/static/js/2.a1e2656b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04e268971ee8f029fd84",
+    "url": "/cartoon/static/js/main.efb26544.chunk.js"
+  },
+  {
+    "revision": "a983544d56992308ebd5",
+    "url": "/cartoon/static/js/runtime-main.66bf13d2.js"
+  }
+]);
